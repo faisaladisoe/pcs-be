30c6d0ed579909751ed4f9b6b6bc52e408eea8c3
M Faisal Adi Soesatyo <38213559+faisaladisoe@users.noreply.github.com>
2022-10-30 19:36:46 +0700

    Update index.js

--- api/v1/check-in-out/index.js
+++ api/v1/check-in-out/index.js
@@ -10,7 +10,7 @@ router.use(bodyParser.json());
 router.get('/:bus_plate', controller.getPassengersNum);
 router.patch('/activate/:bus_plate', controller.changeBusStatusAct);
 router.patch('/deactivate/:bus_plate', controller.changeBusStatusDeact);
-router.patch('/:bus_plate', controller.incPassenger);
-router.patch('/:bus_plate', controller.decPassenger);
+router.patch('/increment/:bus_plate', controller.incPassenger);
+router.patch('/decrement/:bus_plate', controller.decPassenger);
 
 module.exports = router;
